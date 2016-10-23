d7acb9728b7771c506788563ed7afdab2a2132a7
Robert J Cole <robertjcole@yahoo.com>
2016-10-23 18:55:10 -0400

    heroku fix

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(function (req, res, next) {
-//   if (req.headers['x-forwarded-proto'] === 'http') {
-//     next();
-//   } else {
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// });
+app.use(function (req, res, next) {
+  if (req.headers['X-Forwarded-Proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+    //res.redirect(301, ['http://', req.get('Host'), req.url].join(''));
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
